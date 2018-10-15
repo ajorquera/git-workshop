7d8786dd5ec1ac093843bf19519194e9fc8239e7
Linus Torvalds <linus@transmeta.com>
2018-10-15 19:08:15 +0200

    Remove unnecesary code

--- suncalc.js
+++ suncalc.js
@@ -301,11 +301,3 @@ SunCalc.getMoonTimes = function (date, lat, lng, inUTC) {
 
     return result;
 };
-
-
-// export as Node module / AMD module / browser variable
-if (typeof exports === 'object' && typeof module !== 'undefined') module.exports = SunCalc;
-else if (typeof define === 'function' && define.amd) define(SunCalc);
-else window.SunCalc = SunCalc;
-
-}());
